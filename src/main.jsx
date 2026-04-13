6dc2d41ede44188c34f41bad5762173628d67b57
nazia <naziya222005@gmail.com>
2026-04-13 22:59:50 +0530

    initial commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,17 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import App from './App'
+import Landing from './Pages/Landing'
 import './index.css'
-import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Landing />} />
+        <Route path="/dashboard" element={<App />} />
+      </Routes>
+    </BrowserRouter>
+  </StrictMode>
 )
